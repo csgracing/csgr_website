c17489cd76fd3b42ae662b2d2210981382b12173
Calum Breen-Chapman <calumbreenchapman@gmail.com>
2025-11-24 12:17:51 +0000

    wrong size

--- app/components/Navbar/SeasonDropDown.tsx
+++ app/components/Navbar/SeasonDropDown.tsx
@@ -29,7 +29,7 @@ export default function Menu() {
                 <Link href="/teams/2026season" 
                 className="flex px-4
                          text-black dark:text-white
-                           transition duration-300 hover:not-md:text-2xl md:text-xl hover:md:underline decoration-csg-red underline-offset-4">
+                           transition duration-300 hover:not-md:text-2xl md:text-lg hover:md:underline decoration-csg-red underline-offset-4">
                            Season'26
                 </Link>
                 <button
