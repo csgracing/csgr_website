8d18723ef34f0843da91cd82de0b625615d22ee7
Calum Breen-Chapman <calumbreenchapman@gmail.com>
2025-12-03 14:03:41 +0000

    link fix

--- app/components/images/logos/logos.tsx
+++ app/components/images/logos/logos.tsx
@@ -17,11 +17,11 @@ import Igus from "./sponsors/igus.svg";
 import Hoosier from "./sponsors/hoosier.svg";
 
 export const SponsorCarouselLogos = [
-  { src: Vector , link: "https://www.vector.com/" },
-  { src: Ansys , link: "https://www.ansys.com/" },
-  { src: Altair , link: "Lhttps://altair.com/" },
-  { src: Mathworks , link: "https://uk.mathworks.com/" },
-  { src: EasyComposites , link: "https://www.easycomposites.co.uk/" },
-  { src: Igus , link: "https://www.igus.co.uk/" },
-  { src: Hoosier , link: "https://www.hoosiertire.com/" }
+  { src: Vector , link: "https://www.vector.com/",desc:"Vector software tools offer system architects, network designers, development and test engineers comprehensive support for the entire development process: from system design and integration to testing, calibration and diagnostics of single components or the entire system." },
+  { src: Ansys , link: "https://www.ansys.com/", desc:"Ansys engineering simulation and 3D design software delivers product modeling solutions with unmatched scalability"},
+  { src: Altair , link: "https://altair.com/", desc:"When data science meets rocket science, incredible things happen. The innovation our world-changing technology enables may feel like magic to users, but it’s the time-tested result of the rigorous application of science, math, and Altair."},
+  { src: Mathworks , link: "https://uk.mathworks.com/",desc:"101112abc" },
+  { src: EasyComposites , link: "https://www.easycomposites.co.uk/",desc:"131415abc" },
+  { src: Igus , link: "https://www.igus.co.uk/",desc:"161718abc" },
+  { src: Hoosier , link: "https://www.hoosiertire.com/",desc:"192021abc" }
 ];
