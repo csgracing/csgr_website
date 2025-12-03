a559434dd2dafccdc4f62affad97ab7a6c1d67c1
Calum Breen-Chapman <calumbreenchapman@gmail.com>
2025-12-03 16:25:34 +0000

    scaling

--- app/components/SponsorCarousel/SponsorGrid.tsx
+++ app/components/SponsorCarousel/SponsorGrid.tsx
@@ -19,14 +19,17 @@ const SponsorGrid = () => {
     };
 
     return(
-        <div className='grid grid-cols-2 xl:grid-cols-3 justify-center items-center gap-4 px-2 py-2'>
+        <div className='md:grid grid-cols-2 xl:grid-cols-3 justify-center items-center gap-4 px-2 py-2'>
         {SponsorCarouselLogos.map((Logo, index) => (
+        <div className='py-2'>
           <div key={index} className='flex flex-col justify-center bg-white dark:bg-black overflow-x-hidden border-4 rounded-2xl border-csg-red h-100 text-center items-center p-4 gap-4' onMouseEnter={() => handleMouseEnter(index)} onMouseLeave={handleMouseLeave}>
             <a href={Logo.link}>
               <Logo.src className='w-full py-2 px-2 fill-black dark:fill-white hover:scale-110 transition duration-300'/>
             </a>
+            <p className='relative text-md lg:hidden'>{Logo.desc}</p>
             {desc && currentIndex === index && (<p className='relative text-md text-slideIn'>{Logo.desc}</p>)}
           </div>
+        </div>
         ))}
       </div>
     )
--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -9,8 +9,8 @@ export default function sponsors() {
 
   return (
     <div className=''>
-    <h1 className='text-4xl text-center py-4 text-csg-red'>OUR SPONSORS</h1>
-    <h2 className='text-4xl text-center py-4 px-10'>We proudly recognize the partners whose support makes our mission possible. Each sponsor plays a vital role in helping the development of our car</h2>
+    <h1 className='text-2xl md:text-4xl text-center py-4 text-csg-red'>OUR SPONSORS</h1>
+    <h2 className='text-lg md:text-2xl text-center py-4 px-10'>We proudly recognize the partners whose support makes our mission possible. Each sponsor plays a vital role in helping the development of our car</h2>
     <SponsorGrid />
     </div>
   );
