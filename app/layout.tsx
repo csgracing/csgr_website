8844022f36ae7881fd3448ccb7a4c3b06d59938a
Calum Breen-Chapman <calumbreenchapman@gmail.com>
2026-02-05 10:13:30 +0000

    font change

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className='font-aharoni antialiased'
+        className='antialiased'
       >
         <link
         rel="apple-touch-icon"
