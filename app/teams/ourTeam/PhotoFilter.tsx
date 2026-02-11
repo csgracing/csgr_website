1ee1608f8d18cf3b7a6278454d334d4af62287a6
Calum Breen-Chapman <calumbreenchapman@gmail.com>
2026-02-11 00:59:15 +0000

    putting teamMembers and photos into a separate file for import

--- app/teams/ourTeam/PhotoFilter.tsx
+++ app/teams/ourTeam/PhotoFilter.tsx
@@ -1,24 +1,8 @@
 'use client'
 import Image from 'next/image';
 import React from 'react';
+import {teamMembers,departments} from './teamMembers/TeamMembers'
 
-
-import PlaceHolder from './teamMembers/Person_Image.png'
-const teamMembers = [
-  {id:1, Name: "Calum Breen-Chapman", Role: "Web Developer", Image:PlaceHolder ,Department:"Software", tag:["TL","SW"], desc:"Calum is a web developer responsible for designing and maintaining the team's website. He ensures that the site is user-friendly, visually appealing, and up-to-date with the latest information about the team and its activities."},
-  {id:2, Name: "Calum Breen-Chapman", Role: "Web Developer", Image:PlaceHolder ,Department:"Software", tag:["SW"], desc:"Calum is a web developer responsible for designing and maintaining the team's website. He ensures that the site is user-friendly, visually appealing, and up-to-date with the latest information about the team and its activities."},
-];
-const departments=[
-  {id:"TL", name:"Team Leads"},
-  {id:"SW", name:"Software"},
-  {id:"ME", name:"Mechanical"},
-  {id:"EV", name:"Electrical"},
-  {id:"AE", name:"Aerodynamics"},
-  {id:"CH", name:"Chassis"},
-  {id:"VD", name:"SSB & Vehicle Dynamics"},
-  {id:"MD", name:"Media & Marketing"},
-  {id:"BS", name:"Business"},
-];
 
 export default function PhotoFilter() {
 
@@ -54,8 +38,6 @@ function handleSelect(option: string){
         </div>
       ))}
       </div>
-      
-      
     </div>
   );
 }
---
+++ app/teams/ourTeam/teamMembers/TeamMembers.tsx
@@ -0,0 +1,18 @@
+import PlaceHolder from './Person_Image.png'
+
+export const teamMembers = [
+  {id:1, Name: "Calum Breen-Chapman", Role: "Web Developer", Image:PlaceHolder ,Department:"Software", tag:["TL","SW"], desc:"Calum is a web developer responsible for designing and maintaining the team's website. He ensures that the site is user-friendly, visually appealing, and up-to-date with the latest information about the team and its activities."},
+  {id:2, Name: "Calum Breen-Chapman", Role: "Web Developer", Image:PlaceHolder ,Department:"Software", tag:["SW"], desc:"Calum is a web developer responsible for designing and maintaining the team's website. He ensures that the site is user-friendly, visually appealing, and up-to-date with the latest information about the team and its activities."},
+];
+
+export const departments=[
+  {id:"TL", name:"Team Leads"},
+  {id:"SW", name:"Software"},
+  {id:"ME", name:"Mechanical"},
+  {id:"EV", name:"Electrical"},
+  {id:"AE", name:"Aerodynamics"},
+  {id:"CH", name:"Chassis"},
+  {id:"VD", name:"SSB & Vehicle Dynamics"},
+  {id:"MD", name:"Media & Marketing"},
+  {id:"BS", name:"Business"},
+];
